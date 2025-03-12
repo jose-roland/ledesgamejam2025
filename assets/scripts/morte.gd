f1e031b439760ddb938aad8cfa39c298495a6d00
José Roland <jose.sroland@gmail.com>
2025-03-12 15:28:24 -0400

    :sparkles: feat: Morte

--- assets/scripts/morte.gd
+++ assets/scripts/morte.gd
@@ -1,15 +1,10 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	print("teste")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()

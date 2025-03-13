c198847b9a76c67674dbc13067cd51e14e060b4b
José Roland <jose.sroland@gmail.com>
2025-03-12 21:55:34 -0400

    :sparkle: feat: Dash :card_file_box: raw: Mudança na velocidade do personagem

--- assets/scripts/personagem.gd
+++ assets/scripts/personagem.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 128.0
+const JUMP_VELOCITY = -256.0
+const DASH_SPEED = 512.0
+var dash = false
+var can_dash = true
+
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_timer_2: Timer = $DashTimer2
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +17,33 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("dash") && can_dash:
+		dash = true
+		can_dash = false
+		dash_timer.start()
+		dash_timer_2.start()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		if dash:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	dash = false
+
+
+
+func _on_dash_timer_2_timeout() -> void:
+	can_dash = true

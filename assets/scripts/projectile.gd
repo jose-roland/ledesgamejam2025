4c8083bc14588dd113d6c21028e2d4694111151f
Droppas <matheus.droppa@ufms.br>
2025-03-16 19:54:14 -0400

    🐛 fix: Projectile e organização Assets
    Imposição de um limite para o projétil assim que ele sair da tela do player, organização do Assets em pastas

--- assets/scripts/projectile.gd
+++ assets/scripts/projectile.gd
@@ -7,7 +7,6 @@ var direction := 1
 
 func _process(delta: float) -> void:
 	position.x += SPEED * delta * direction
-	print("Projétil posição:", position)
 	if direction < 0:
 		$AnimatedSprite2D.set_flip_h(true)
 	else:
@@ -18,4 +17,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("inimigos"):
 		body.levar_dano()
+	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()

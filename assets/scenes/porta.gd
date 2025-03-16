08727a50b0b5dc97ecbae956971104c7a4be9f9f
GabrielFernando <gabrielfariaferreira19@gmail.com>
2025-03-16 11:40:02 -0400

    :sparkles: feat: Transição dos níveis

---
+++ assets/scenes/porta.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var on_door = false
+
+func _on_body_entered(body: Node2D) -> void:
+	on_door = true
+
+func _on_body_exited(body: Node2D) -> void:
+	on_door = false
+
+func _process(delta: float) -> void:
+	if on_door == true and Input.is_action_just_pressed("Entrar na porta"):
+		get_tree().change_scene_to_file("res://assets/scenes/Nivel_2.tscn")

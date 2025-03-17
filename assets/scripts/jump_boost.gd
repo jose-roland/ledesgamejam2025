eed4cd10a2aeef8275494639ba3212a0ed42f849
José Roland <jose.sroland@gmail.com>
2025-03-16 23:44:05 -0400

    :sparkles: feat: SFX para boosters
    :sparkles: feat: Boosters no mapa

--- assets/scripts/jump_boost.gd
+++ assets/scripts/jump_boost.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @export var item: InvItem
+@onready var audio_moeda: AudioStreamPlayer2D = $"../../Personagem/Audio_Moeda"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	body.collect(item)
+	audio_moeda.play()
 	body.increase_jump_velocity(16.0)
 	queue_free()
--- assets/scripts/moeda.gd
+++ assets/scripts/moeda.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 @export var item: InvItem
+@onready var audio_moeda: AudioStreamPlayer2D = $"../../Personagem/Audio_Moeda"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	$"../../Personagem/Audio_Moeda".play()
+	audio_moeda.play()
 	body.collect(item)
 	queue_free()
 	pass # Replace with function body.
